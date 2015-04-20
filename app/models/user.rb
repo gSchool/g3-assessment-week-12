6e519ebf3eec680ae7bb86a3e011af58c1785035
Jake Marwil <rjm02006@gmail.com>
2015-04-20 14:01:14 -0600

    add has_secure_password to model and password_digest column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 
     has_many :reviews
 
+    has_secure_password
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420170141) do
+ActiveRecord::Schema.define(version: 20150420195958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150420170141) do
   create_table "users", force: true do |t|
     t.string "username"
     t.string "password"
+    t.string "password_digest"
   end
 
 end
