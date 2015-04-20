c9b2a5e35b468eaa25a0276d63b08ccbb2c76070
Jordan Collier <jordanicollier@gmail.com>
2015-04-20 11:57:54 -0600

    simplify relationships and get rid of join table

--- db/migrate/20150420153258_create_movie_reviews.rb
+++
@@ -1,9 +0,0 @@
-class CreateMovieReviews < ActiveRecord::Migration
-  def change
-    create_table :movie_reviews do |t|
-      t.belongs_to :review, index: true
-      t.belongs_to :movie, index: true
-      t.belongs_to :user, index: true
-    end
-  end
-end
--- db/migrate/20150420173950_create_reviews.rb
+++ db/migrate/20150420173950_create_reviews.rb
@@ -3,6 +3,8 @@ class CreateReviews < ActiveRecord::Migration
     create_table :reviews do |t|
       t.string :title
       t.text :body
+      t.belongs_to :user, index: true
+      t.belongs_to :movie, index: true
     end
   end
 end
