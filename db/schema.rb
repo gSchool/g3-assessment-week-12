22bf3200cc7bac921579ffa346a6fa15dfa43768
John Marty <JohnRmarty@gmail.com>
2015-04-20 11:13:46 -0600

    migration for movies and reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420154446) do
+ActiveRecord::Schema.define(version: 20150420171238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: true do |t|
+    t.string  "movie_name"
+    t.integer "year"
+    t.string  "synopsis"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.string  "title"
+    t.string  "body"
+    t.date    "date"
+    t.integer "user_id"
+    t.integer "movie_id"
+  end
 
   create_table "users", force: true do |t|
     t.string "username"
