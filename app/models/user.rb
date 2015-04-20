df325f144abbed96092b12dcf09b36c40b52f364
Alexandra Hanley <alexandrahanley@gmail.com>
2015-04-20 13:07:17 -0600

    user can sign up and sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
+
   has_many :reviews
   has_many :movies, through: :reviews
+
+  has_secure_password
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
 root 'welcome#index'
 
+  get '/sign-up' => 'users#new', as: :signup
+  post '/sign-up' => 'users#create'
+  get 'sign-in' => 'sessions#new', as: :signin
+  post '/sign-in' => 'sessions#create'
+  get '/sign-out' => 'sessions#destroy', as: :signout
+
 resources :users
 resources :movies do
   resources :reviews
