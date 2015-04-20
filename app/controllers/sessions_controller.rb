90d740c4a0981f516b28bdc23123f6c4ecd0693c
Kenneth To <kennethkto@gmail.com>
2015-04-20 11:59:04 -0600

    add more fixes to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.find(params[:id])
+    @user = User.new
   end
 
 
   def create
-    @user = User.find(user_name: params[:user_name])
+    @user = User.find_by(user_name: params[:user_name])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/login'
