0390f8e121b80e277025e4b9eeb1d0953da5d270
Alexandra Hanley <alexandrahanley@gmail.com>
2015-04-20 11:35:09 -0600

    user can delete a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,6 +34,11 @@ class MoviesController < ApplicationController
   end
 end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, notice: 'Movie was deleted.'
+  end
 
 private
 
