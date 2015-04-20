e9f0a6b10e53762ec9db482162130b42ee8168dc
Danielle <danielle@daniellemullane.com>
2015-04-20 10:17:42 -0600

    Add registrations controller and view, add sessions controller and view, and add routes for each

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :users
 
   root 'home#index'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+  get '/signup' => 'registrations#new'
+  post '/signup' => 'registrations#create'
+
 
 
 end
