2f8c5dea37e112e82411ed990eb165bdad3292c6
CaseyKelly <casey.kelly@colorado.edu>
2015-04-20 10:32:27 -0600

    add reviews to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
