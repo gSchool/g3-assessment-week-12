e0d2d93e04bcf13b125d14147c85bc88363e5d60
nina <ninajl@gmail.com>
2015-04-20 10:51:42 -0600

    Created login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   def new
   end
 
-  
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        redirect_to signup_path
+      end
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,3 @@
 class WelcomeController < ApplicationController
-
+  before_action :authorize
 end
