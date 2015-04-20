941cb5b681157a1bd8c3afcd4dab2f40a8200087
Reyna DeLoge <reyna.deloge@gmail.com>
2015-04-20 10:45:43 -0600

    users can signup, signin, and logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420154829) do
+ActiveRecord::Schema.define(version: 20150420162513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150420154829) do
     t.string  "title"
     t.integer "year"
     t.text    "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
