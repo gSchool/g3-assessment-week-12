5d4e3e966771dc35fa317255ed1eb50acc854164
Jake Marwil <rjm02006@gmail.com>
2015-04-20 13:34:44 -0600

    modify create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
-  
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.find_by(email: params[:user][:email])
+    @user = User.find_by(username: params[:user][:username])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      @user = User.new(email: params[:user][:email])
+      @user = User.new(username: params[:user][:username])
       @user.errors[:base] << "Username / password is invalid"
       render :new
     end
