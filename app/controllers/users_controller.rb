afad6ded0e16589a70d2c1203be50d05efd077bf
Peter Vliet <petervliet@gmail.com>
2015-04-20 10:21:25 -0600

    user is logged in upon password creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(params.require(:user).permit(:username, :password))
 
     if @user.save
+      session[:current_user] = @user.id
       redirect_to root_path, notice: 'user was created'
     else
       render :new
