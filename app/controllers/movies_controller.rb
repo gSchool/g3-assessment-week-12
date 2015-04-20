0d1a8950910204f1af22c752f5dcb6246a1afda1
Melissa Khat <melissa.khat@gmail.com>
2015-04-20 13:18:32 -0600

    render new movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,37 @@
 class MoviesController < ApplicationController
-  def index
-  end
+   def index
+      @movies = Movie.all
+   end
 
-  def new
-     @movie = Movie.new(params[:id])
-  end
+   def new
+      @movie = Movie.new(params[:id])
+   end
 
-  def create
-     @movie = Movie.create(movie_params)
-     
-  end
+   def create
+      @movie = Movie.new(params[:id])
+      if @movie.save
+         redirect_to movies_path
+      else
+         render :new
+      end
+   end
+
+   def show
+      @movie = Movie.find(params[:id])
+   end
 
 
-private
-def movie_params
-   params.require(:movie).permit(:name, :year, :synopsis)
+
+
+   private
+
+   def set_movie
+      @movie = Movie.find(params[:id])
+   end
+
+   def movie_params
+      params.require(:movie).permit(:name, :year, :synopsis)
+   end
+
+
 end
