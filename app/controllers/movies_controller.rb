9caa96b95bef4e47fe28864722f01cb8be3d2619
Melissa Khat <melissa.khat@gmail.com>
2015-04-20 10:26:10 -0600

    new movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,18 @@
 class MoviesController < ApplicationController
   def index
   end
+
+  def new
+     @movie = Movie.new(params[:id])
+  end
+
+  def create
+     @movie = Movie.create(movie_params)
+     
+  end
+
+
+private
+def movie_params
+   params.require(:movie).permit(:name, :year, :synopsis)
 end
