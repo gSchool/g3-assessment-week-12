719c46c853a9445bfd18763771f90f7bf8bbd250
CaseyKelly <casey.kelly@colorado.edu>
2015-04-20 10:04:20 -0600

    add movies migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420153042) do
+ActiveRecord::Schema.define(version: 20150420160344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: true do |t|
+    t.string "name"
+    t.date   "year"
+    t.text   "synopsis"
+  end
 
   create_table "users", force: true do |t|
     t.string "username"
