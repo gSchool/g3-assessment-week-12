cc9094a16f533c8dec51732ed8fa42376c3a1228
CaseyKelly <casey.kelly@colorado.edu>
2015-04-20 10:18:42 -0600

    add movies index and update links on welcome index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      redirect_to root_path, notice: "The movie was created"
+      redirect_to movies_path, notice: "The movie was created"
     else
       render :new, notice: "It didn't work :-("
     end
