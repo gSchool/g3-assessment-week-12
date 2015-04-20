d464679c172ec7bf2c61c6ed61c59c247741dd8f
Peter Vliet <petervliet@gmail.com>
2015-04-20 13:22:18 -0600

    set up review as join table between movie and user

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,5 @@ class Movie < ActiveRecord::Base
   validates :title, presence: true
   validates :year, presence: true, numericality: true
 
+  has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
 
+  belongs_to :movie
+  belongs_to :user
 
 end
