9440682846376a652832a22864c88e21fce224ac
Jordan Collier <jordanicollier@gmail.com>
2015-04-20 13:20:21 -0600

    can create a review with a collection select

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,8 +10,8 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @movie = Movie.find(params[:movie_id])
-    @review.movie_id = @movie.id
+    # @movie = Movie.find(params[:movie_id])
+    # @review.movie_id = @movie.id
     @review.user_id = @current_user.id
     if @review.save
       redirect_to root_path
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
+  validates_presence_of :title, :body, :movie_id
   belongs_to :movie
   belongs_to :user
 end
