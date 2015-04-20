f3e5257760a088c87be2ce76cc450da994c8555d
Melissa Khat <melissa.khat@gmail.com>
2015-04-20 16:49:37 -0600

    edit signin/signup pages

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -4,14 +4,15 @@ class RegistrationController < ApplicationController
 
 
   def new
+     @user = User.new
   end
-  
+
   def create
      @user = User.new(user_params)
      if @user.save
         session[:user_id] = @user.id
         redirect_to movies_path
-     else
+     else 
         render :new
      end
   end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,12 @@
 class SessionController < ApplicationController
 
-  def create
-     @user = User.find(params[:username])
-     @user.authenticate(params[:password])
-     session[:user_id] = @user.id
-
-  end
+   def create
+      @user = User.find(params[:username])
+      if user && user.authenticate(params[:password])
+         session[:user_id] = @user.id
+         redirect_to movies_path
+      else
+         render :new
+      end
+   end
 end
