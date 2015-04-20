ed21a37b7f17002062b42c7c654da0e60d8903d4
nina <ninajl@gmail.com>
2015-04-20 16:53:25 -0600

    Fixed a spelling error

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ActiveRecord::Base
   has_many  :reviews
   has_many  :users, through: :reviews
 
-  validate :title, :about,  presence:true
-  validate :year, numericality: { only_integer: true}, presence: true
+  validates :title, :about,  presence:true
+  validates :year, numericality: { only_integer: true}, presence: true
 end
