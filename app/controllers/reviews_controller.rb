a1b5d842a72cf773995d8b41801d4ddd934d6bf2
Jake Marwil <rjm02006@gmail.com>
2015-04-20 10:11:23 -0600

    add index new and create methods as well as strong params

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path, notice: "Review was successfully created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:text)
+  end
+
+end
