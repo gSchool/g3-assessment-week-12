6c266f458e24b2e3d1173e0f65babf1a02c139b2
Wesley Merkel <ooesili@gmail.com>
2015-04-20 11:32:54 -0600

    Allow users to review movies

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,39 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
   end
 
+  def new
+    @review = Review.new
+    set_hidden_field
+  end
+
+  def create
+    @movie = Movie.find(review_params[:movie_id])
+    @review = @movie.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to reviews_path, notice: 'Review successfully created'
+    else
+      set_hidden_field
+      flash.now[:alert] = 'There was a problem creating the review'
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :body, :movie_id)
+  end
+
+  def set_hidden_field
+    if params[:movie_id]
+      @movie = Movie.find(params[:movie_id])
+      @review.movie = @movie
+    else
+      @movie = nil
+      @review.movie_id = nil
+      @movies = Movie.all
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
-  resources :movies, only: [:index, :new, :create]
-  resources :reviews, only: [:index]
+  resources :movies, only: [:index, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:index, :new, :create]
 end
