f6c53345c1d3d99acab2326d9ba21e3f772e95bf
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-04-20 13:59:39 -0600

    Users can review movies from movie index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,11 +6,18 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    if params[:movie_id]
+    @movie = Movie.find(params[:movie_id])
+    end
   end
 
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    if params[:movie_id]
+    @movie = Movie.find(params[:movie_id])
+    @review.movie_id = @movie.id
+    end
     if @review.save
       redirect_to reviews_path
     else
