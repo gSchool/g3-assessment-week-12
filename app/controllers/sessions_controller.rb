64d60744056853c804285dbd9e833b705be2fa42
Jessica <jessicakoch136@icloud.com>
2015-04-20 13:31:37 -0600

    fix logout and session controller bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController
+class SessionsController < ApplicationController
   def new
     @user = User.new
   end
@@ -20,7 +20,7 @@ class SessionsController
   end
 
   def destroy
-    session.delete
+    session.clear
     redirect_to root_path, notice:"Come again soon!"
   end
 end
