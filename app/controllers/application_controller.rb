50be9ad03be6543eb48b2c65d35c010a3a84b070
Jake Marwil <rjm02006@gmail.com>
2015-04-20 13:26:29 -0600

    remove ensure current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :ensure_current_user
-
-  def ensure_current_user
-    redirect_to signin_path unless current_user
-  end
-
   def current_user
     User.find_by(id: session[:user_id])
   end
