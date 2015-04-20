a318965ffbb2cfb1b32e611f940dbb22d94e0476
Alexandra Hanley <alexandrahanley@gmail.com>
2015-04-20 11:19:06 -0600

    user can edit and update a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,10 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
@@ -20,6 +24,16 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render :new
+  end
+end
+
 
 private
 
