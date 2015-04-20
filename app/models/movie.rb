674d17e0a81399dbfdc620d15c769e2a1a8b75b2
Jake Marwil <rjm02006@gmail.com>
2015-04-20 09:58:50 -0600

    add presence: true to validates line

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
 
   has_many :reviews
 
-  validates :name, :year, :synopsis
+  validates :name, :year, :synopsis, presence: true
   validate :year_is_number
 
   def year_is_number
