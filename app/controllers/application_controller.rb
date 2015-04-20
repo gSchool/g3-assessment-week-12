c9a7e50454c7942ef65e6e7102fb68659a9b1350
Jessica <jessicakoch136@icloud.com>
2015-04-20 13:56:26 -0600

    add bcrypt gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :log_in
   helper_method :logged_in?
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
   end
 
   def logged_in?
