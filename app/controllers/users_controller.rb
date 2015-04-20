cbdb36d4ba0da90a457c8762b3f1f85866305fd8
Kenneth To <kennethkto@gmail.com>
2015-04-20 10:35:33 -0600

    fix syntax in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
-class User
+class UsersController < ApplicationController
   def new
-
+    @user = User.new(user_params)
   end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+private
+
+def user_params
+  params.require(:user).permit(:user_name, :password, :id)
+end
 
 end
