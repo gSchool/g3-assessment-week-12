df33111132b1e2619eea202bc363327afd274b66
Peter Vliet <petervliet@gmail.com>
2015-04-20 13:11:30 -0600

    review movie_id set different ways depending what route user is on

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,11 +6,17 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    unless request.fullpath == '/reviews/new'
+      @movie = Movie.find(params[:movie_id])
+    end
   end
 
   def create
     @review = Review.new(params.require(:review).permit(:title, :body, :movie_id, :user_id, :created_at))
-    @review.user_id = current_user.id
+      @review.user_id = current_user.id
+      if @review.movie_id == nil
+        @review.movie_id = Movie.find(params[:movie_id]).id
+      end
     if @review.save
       redirect_to movies_path
     else
