c95a58d730de6b77358c959a6ea8dc36460ddba3
Jake Marwil <rjm02006@gmail.com>
2015-04-20 11:04:33 -0600

    add users table and add user_id and date column to reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420162804) do
+ActiveRecord::Schema.define(version: 20150420170141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20150420162804) do
     t.integer "movie_id"
     t.string  "title"
     t.text    "body"
+    t.date    "date"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "username"
+    t.string "password"
   end
 
 end
