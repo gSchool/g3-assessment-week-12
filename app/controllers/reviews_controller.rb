7abf1959651f6c49e5eaa9122788f373b88ed25a
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-04-20 13:15:46 -0600

    Current user is now associated with their reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,6 +9,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
     if
       @review.save
       redirect_to reviews_path
