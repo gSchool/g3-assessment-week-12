bb815179f9da96209d2ec3be707835c2f0094ae0
John Marty <JohnRmarty@gmail.com>
2015-04-20 09:51:31 -0600

    sessions controller new-create-destroy, user migration with password digest and username,bycrypt-gem, user controller and user model

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      @user = User.new(username: params[:user][:username])
+      @user.errors[:base] << "Username / password is invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to signin_path
+  end
+
+end
