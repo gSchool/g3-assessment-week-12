b92adbff14a67808d9135aa4ac9331b71280eb77
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-04-20 10:00:50 -0600

    Users can sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
    get '/login' => 'sessions#new'
    post '/login' => 'sessions#create'
 
+   resources :users, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
