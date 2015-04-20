75292cb86a319d12c7bf4e5a345fdf55fab6c99c
Daniel Harris <bobth3bum@gmail.com>
2015-04-20 10:49:12 -0600

    change welcome page to not show login and signup when user is logged in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
 
   def index
-    @review = Review.all.sort_by(&:created_at)
+    @reviews = Review.all.sort_by(&:created_at)
   end
 
   def new
@@ -9,12 +9,15 @@ class ReviewsController < ApplicationController
   end
 
   def create
-
-  end
-
-  private
-
-  def review_params
-    params.require(:review).permit(:title, :body)
+    @review = Review.new(title: params[:title], body: params[:body])
+    @movie = Movie.find_by(id: params[:movie][:id])
+    if @movie
+      @review.movie_id = @movie.id
+    end
+    if @review.save
+      redirect_to reviews_path, notice: "Review saved"
+    else
+      render :new, notice: "Incorrect data"
+    end
   end
 end
