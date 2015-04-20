af718209a326064ec3a084d20c7a77acc4d68143
CaseyKelly <casey.kelly@colorado.edu>
2015-04-20 10:07:20 -0600

    add movies controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,22 @@
+class MoviesController < ApplicationController
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to root_path, notice: "The movie was created"
+    else
+      render :new, notice: "It didn't work :-("
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :synopsis)
+  end
+
+end
