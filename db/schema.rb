c98e94cf1c4bd3cfa7c56465d63753fd5f4a3f4c
Hayley Blackstock <hayleyblackstock@gmail.com>
2015-04-20 09:47:06 -0600

    update password in users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420154332) do
+ActiveRecord::Schema.define(version: 20150420154634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20150420154332) do
 
   create_table "users", force: true do |t|
     t.string "username"
-    t.string "password_digest"
+    t.string "password"
   end
 
 end
