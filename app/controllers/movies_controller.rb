5da03369fdf7cb73b5f425e6cc9dfefffe277433
Alexandra Hanley <alexandrahanley@gmail.com>
2015-04-20 11:02:35 -0600

    user can add a new movie

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,30 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :synopsis)
+  end
+
+end
