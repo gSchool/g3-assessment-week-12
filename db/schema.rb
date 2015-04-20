df10db886b0e7d4933a7e2f32ba4d9caffa5ff06
Jessica <jessicakoch136@icloud.com>
2015-04-20 16:13:03 -0600

    update sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420211849) do
+ActiveRecord::Schema.define(version: 20150420215501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20150420211849) do
 
   create_table "users", force: true do |t|
     t.string "username"
-    t.string "password"
     t.string "password_digest"
   end
 
